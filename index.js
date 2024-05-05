d5d28225ddb7df920bea61cc3e3f25721a5062e2
shaheer417 <niazi2822@gmail.com>
2024-05-06 01:28:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const input1 = await inquirer.prompt({
+    name: "num1",
+    type: "number",
+    message: "Enter the number",
+});
+const number = 5;
+if (input1.num1 === number) {
+    console.log(`You have guess the right number ${number}`);
+}
+else {
+    console.log("Please try agin");
+}
